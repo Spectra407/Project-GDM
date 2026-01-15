3d9de5b717113c5aa4733240b7baaae0ac75e22f
Spectra407 <realpokebey@gmail.com>
2026-01-14 22:48:13 -0500

    Progressed UI and brainstorming card data script.

---
+++ Assets/Scripts/CardVisual.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+public class CardVisual : MonoBehaviour
+{
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text description;
+    [SerializeField] private TMP_Text madness;
+    [SerializeField] private SpriteRenderer imageSR;
+    [SerializeField] private GameObject wrapper;
+}
