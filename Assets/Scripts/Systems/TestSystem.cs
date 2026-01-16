929623952731ff7978bf090d9926934286c82eeb
Spectra407 <realpokebey@gmail.com>
2026-01-15 21:34:30 -0500

    Finished HandView, CardView, and CardViewCreator scripts. Setup a temporary prefab for the CardView.

---
+++ Assets/Scripts/Systems/TestSystem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestSystem : MonoBehaviour
+{
+    [SerializeField] private HandView handView;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            CardView cardView = CardViewCreator.Instance.CreateCardView(transform.position, Quaternion.identity);
+            StartCoroutine(handView.AddCard(cardView));
+        }
+    }
+}
--- Assets/Scripts/Views/CardView.cs
+++ Assets/Scripts/Views/CardView.cs
@@ -1,9 +1,9 @@
 using TMPro;
 using UnityEngine;
 
-public class CardVisual : MonoBehaviour
+public class CardView : MonoBehaviour
 {
-    [SerializeField] private TMP_Text title;
+    
     [SerializeField] private TMP_Text description;
     [SerializeField] private TMP_Text madness;
     [SerializeField] private SpriteRenderer imageSR;
