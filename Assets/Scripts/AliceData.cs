fae975fef74e66ccafc73c7d79c32c34b04a73ef
Aurélien <fuying.breant@gmail.com>
2026-01-25 17:15:33 -0500

    Create Alice Scene & AliceData Script
    Script contains Alice's stats (max helath, defense, buttons...)
    
    Also contains an "AliceTakeDamage" function, which lowers Alice's health when it is called; accounts for defense/block.

--- Assets/Scripts/AliceData.cs
+++ Assets/Scripts/AliceData.cs
@@ -5,17 +5,14 @@ public class AliceData : MonoBehaviour
     public int maxHealth;
     public int currentHealth;
     public int defense;
+    public int maxMadness;
     public int buttons;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        maxHealth = 100;
-        currentHealth = maxHealth;
-        Debug.Log("Current Health " + currentHealth);
-        TakeDamage(30);
-        Debug.Log("Current Health " + currentHealth);
-        
+        maxHealth = 50;
+        maxMadness = 7;    
     }
 
     // Update is called once per frame
@@ -24,7 +21,7 @@ public class AliceData : MonoBehaviour
         
     }
 
-    public void TakeDamage(int damage)
+    public void AliceTakeDamage(int damage)
     {
         if (defense <= damage)
         {
