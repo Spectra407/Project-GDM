8661df3ef2e8de71d4d698f32d3676232a5e7df0
Aurélien <fuying.breant@gmail.com>
2026-01-29 17:18:05 -0500

    Create EnemyData Script
    Add base stats and sprite

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewEnemy", menuName = "Enemies/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    [Header("Stats")]
+    public string enemyName;
+    public int maxHealth;
+
+    [Header("Sprite")]
+    public Sprite enemySprite;
+}
