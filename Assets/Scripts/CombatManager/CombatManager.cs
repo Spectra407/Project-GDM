1fe110ff4fc431f866ff91e79be93437b6045f64
amatdsotm <benjamin.carpiomak@gmail.com>
2026-02-26 13:48:20 -0500

    stashing

--- Assets/Scripts/CombatManager/CombatManager.cs
+++ Assets/Scripts/CombatManager/CombatManager.cs
@@ -9,6 +9,7 @@ public class CombatManager : MonoBehaviour
     public int currentHealth;
     public int tempDefense;
     public int madness;
+    public bool jackpot;
     //maybe move these into alice later
     public int strength;
     public int poison;
@@ -49,6 +50,7 @@ public class CombatManager : MonoBehaviour
         currentHealth = alice.currentHealth;
         madness = alice.startingMadness;
         enemyCurrentHealth = enemy.maxHealth;
+        jackpot = false;
 
         // Kick off the game loop
         MoveToNewState("EnemyChooseActionState");
