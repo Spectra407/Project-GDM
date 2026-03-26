6c58aefd1b31cc55eb40e86ae1fcffaf2cbcd5f8
Elle Adali <eliana.adali06@gmail.com>
2026-03-25 21:29:18 -0400

    credits

---
+++ Assets/Scripts/CreditsSceneController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsSceneController : MonoBehaviour
+{
+    public GameObject[] slides;
+    public float secsPerSlide;
+    public float fadeTime;
+    public float pauseTime;
+
+    private float period;
+    private float time;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        period = secsPerSlide + fadeTime + pauseTime;
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        int slide = (int) (time / period);
+        float phase = time % period;
+
+        if (slide >= slides.Length)
+        {
+            SceneManager.LoadScene("TitleScreenScene");
+        }
+
+        for (int i = 0; i < slides.Length; i++)
+        {
+            slides[i].SetActive(false);
+        }
+
+        float alpha = Math.Min((phase - pauseTime / 2) / (fadeTime / 2),
+                               (period - phase - pauseTime / 2) / (fadeTime / 2));
+        if (alpha < 0) alpha = 0;
+        if (alpha > 1) alpha = 1;
+
+        GameObject current = slides[slide];
+        current.GetComponent<CanvasGroup>().alpha = alpha;
+        current.SetActive(true);
+    }
+}
